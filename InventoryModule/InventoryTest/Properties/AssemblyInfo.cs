7d0c9e54f020560dd1dbd79db47aaed3e1b794c6
rhushi <rhushikeshbaradkar@gmail.com>
2020-11-22 22:56:42 +0530

    test project commited

---
+++ InventoryModule/InventoryTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InventoryTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InventoryTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("deb345d3-9b4f-459c-8646-66a124be0a06")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ InventoryModule/InventoryTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InventoryModule.Controllers;
+using System.Web.Mvc;
+
+namespace InventoryTest
+{
+    [TestClass]
+    public class InventoriesTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            InventoriesController obj = new InventoriesController();
+            var result = obj.Index() as ViewResult;
+            Assert.AreEqual("Index",result.ViewName);
+        }
+        [TestMethod]
+        public void TestCreate()
+        {
+        }
+    }
+}
